0f0bda90725ea19aea89debf800b71800b4119a4
GitHub Action <action@github.com>
2020-08-11 10:13:58 +0000

    style: isort/black

--- tests/test_nn_binarized_linear.py
+++ tests/test_nn_binarized_linear.py
@@ -17,72 +17,45 @@ def fix_seed():
 
 forward_test_case = [
     # (device, test_input, test_bias, test_mode, exptected_shape)
-    ("cpu",
-     torch.rand((1, 10)),
-     False,
-     "deterministic",
-     (1, 20)
-     ),
-
-    (torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-     torch.rand((1, 10)),
-     False,
-     "deterministic",
-     (1, 20)
-     ),
-
-    ("cpu",
-     torch.rand((1, 10)),
-     True,
-     "deterministic",
-     (1, 20)
-     ),
-
-    (torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-     torch.rand((1, 10)),
-     True,
-     "deterministic",
-     (1, 20)
-     ),
-
-    ("cpu",
-     torch.rand((1, 10)),
-     False,
-     "stochastic",
-     (1, 20)
-     ),
-
-    (torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-     torch.rand((1, 10)),
-     False,
-     "stochastic",
-     (1, 20)
-     ),
-
-    ("cpu",
-     torch.rand((1, 10)),
-     True,
-     "stochastic",
-     (1, 20)
-     ),
-
-    (torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-     torch.rand((1, 10)),
-     True,
-     "stochastic",
-     (1, 20)
-     ),
+    ("cpu", torch.rand((1, 10)), False, "deterministic", (1, 20)),
+    (
+        torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        torch.rand((1, 10)),
+        False,
+        "deterministic",
+        (1, 20),
+    ),
+    ("cpu", torch.rand((1, 10)), True, "deterministic", (1, 20)),
+    (
+        torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        torch.rand((1, 10)),
+        True,
+        "deterministic",
+        (1, 20),
+    ),
+    ("cpu", torch.rand((1, 10)), False, "stochastic", (1, 20)),
+    (
+        torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        torch.rand((1, 10)),
+        False,
+        "stochastic",
+        (1, 20),
+    ),
+    ("cpu", torch.rand((1, 10)), True, "stochastic", (1, 20)),
+    (
+        torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        torch.rand((1, 10)),
+        True,
+        "stochastic",
+        (1, 20),
+    ),
 ]
 
 
-@ pytest.mark.parametrize("device, test_input, test_bias, test_mode, exptected_shape",
-                          forward_test_case)
-def test_forward(fix_seed,
-                 device,
-                 test_input,
-                 test_bias,
-                 test_mode,
-                 exptected_shape):
+@pytest.mark.parametrize(
+    "device, test_input, test_bias, test_mode, exptected_shape", forward_test_case
+)
+def test_forward(fix_seed, device, test_input, test_bias, test_mode, exptected_shape):
     test_input = test_input.to(device)
     model = BinarizedLinear(10, 20, bias=test_bias, mode=test_mode).to(device)
 
@@ -90,81 +63,86 @@ def test_forward(fix_seed,
 
 
 clipping_test_case = [
-    ("cpu",
-     torch.rand((1, 10)),
-     False,
-     "determistic",
-     torch.tensor(1.),
-     torch.tensor(-1.)
-     ),
-
-    (torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-     torch.rand((1, 10)),
-     False,
-     "determistic",
-     torch.tensor(1.),
-     torch.tensor(-1.)
-     ),
-
-    ("cpu",
-     torch.rand((1, 10)),
-     True,
-     "determistic",
-     torch.tensor(1.),
-     torch.tensor(-1.)
-     ),
-
-    (torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-     torch.rand((1, 10)),
-     True,
-     "determistic",
-     torch.tensor(1.),
-     torch.tensor(-1.)
-     ),
-
-    ("cpu",
-     torch.rand((1, 10)),
-     False,
-     "stochastic",
-     torch.tensor(1.),
-     torch.tensor(-1.)
-     ),
-
-    (torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-     torch.rand((1, 10)),
-     False,
-     "stochastic",
-     torch.tensor(1.),
-     torch.tensor(-1.)
-     ),
-
-    ("cpu",
-     torch.rand((1, 10)),
-     True,
-     "stochastic",
-     torch.tensor(1.),
-     torch.tensor(-1.)
-     ),
-
-    (torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-     torch.rand((1, 10)),
-     True,
-     "stochastic",
-     torch.tensor(1.),
-     torch.tensor(-1.)
-     ),
+    (
+        "cpu",
+        torch.rand((1, 10)),
+        False,
+        "determistic",
+        torch.tensor(1.0),
+        torch.tensor(-1.0),
+    ),
+    (
+        torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        torch.rand((1, 10)),
+        False,
+        "determistic",
+        torch.tensor(1.0),
+        torch.tensor(-1.0),
+    ),
+    (
+        "cpu",
+        torch.rand((1, 10)),
+        True,
+        "determistic",
+        torch.tensor(1.0),
+        torch.tensor(-1.0),
+    ),
+    (
+        torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        torch.rand((1, 10)),
+        True,
+        "determistic",
+        torch.tensor(1.0),
+        torch.tensor(-1.0),
+    ),
+    (
+        "cpu",
+        torch.rand((1, 10)),
+        False,
+        "stochastic",
+        torch.tensor(1.0),
+        torch.tensor(-1.0),
+    ),
+    (
+        torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        torch.rand((1, 10)),
+        False,
+        "stochastic",
+        torch.tensor(1.0),
+        torch.tensor(-1.0),
+    ),
+    (
+        "cpu",
+        torch.rand((1, 10)),
+        True,
+        "stochastic",
+        torch.tensor(1.0),
+        torch.tensor(-1.0),
+    ),
+    (
+        torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        torch.rand((1, 10)),
+        True,
+        "stochastic",
+        torch.tensor(1.0),
+        torch.tensor(-1.0),
+    ),
 ]
 
 
-@ pytest.mark.parametrize("device, test_input, test_bias, test_mode, exptected_max_value, exptected_min_value",
-                          clipping_test_case)
-def test_clipping(fix_seed,
-                  device,
-                  test_input,
-                  test_bias,
-                  test_mode,
-                  exptected_max_value,
-                  exptected_min_value):
+@pytest.mark.parametrize(
+    "device, test_input, test_bias, test_mode, exptected_max_value, exptected_min_value",
+    clipping_test_case,
+)
+def test_clipping(
+    fix_seed,
+    device,
+    test_input,
+    test_bias,
+    test_mode,
+    exptected_max_value,
+    exptected_min_value,
+):
 
     test_input = test_input.to(device)
     model = BinarizedLinear(10, 20, bias=test_bias, mode=test_mode).to(device)
@@ -175,5 +153,5 @@ def test_clipping(fix_seed,
     model(test_input)
 
     with torch.no_grad():
-        assert(model.weight.min() >= exptected_min_value)
-        assert(model.weight.max() <= exptected_max_value)
+        assert model.weight.min() >= exptected_min_value
+        assert model.weight.max() <= exptected_max_value
--- tests/test_op_binarized_linear.py
+++ tests/test_op_binarized_linear.py
@@ -15,150 +15,126 @@ def fix_seed():
     torch.backends.cudnn.benchmark = False
 
 
-mode_test_case = [(torch.tensor([[1.0, 1.0, 1.0],
-                                 [1.0, 1.0, 1.0],
-                                 [1.0, 1.0, 1.0]]),
-                   torch.tensor([[-1.0, 1.0, 1.0],
-                                 [1.0, -0.8, 1.0],
-                                 [1.0, -0.3, 1.0]]),
-                   None,
-                   "test")]
+mode_test_case = [
+    (
+        torch.tensor([[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]),
+        torch.tensor([[-1.0, 1.0, 1.0], [1.0, -0.8, 1.0], [1.0, -0.3, 1.0]]),
+        None,
+        "test",
+    )
+]
 
 
-@ pytest.mark.parametrize("test_input, test_weight, test_bias, test_mode",
-                          mode_test_case)
-def test_supported_mode(fix_seed,
-                        test_input,
-                        test_weight,
-                        test_bias,
-                        test_mode):
+@pytest.mark.parametrize(
+    "test_input, test_weight, test_bias, test_mode", mode_test_case
+)
+def test_supported_mode(fix_seed, test_input, test_weight, test_bias, test_mode):
     with pytest.raises(RuntimeError):
         binarized_linear(test_input, test_weight, test_bias, test_mode)
 
 
 forward_test_case = [
     # (test_input, test_weight, test_bias, test_mode, expected)
-    (torch.tensor([[1.0, 1.0, 1.0],
-                   [1.0, 1.0, 1.0],
-                   [1.0, 1.0, 1.0]]),
-     torch.tensor([[-1.0, 1.0, 1.0],
-                   [1.0, -0.8, 1.0],
-                   [1.0, -0.3, 1.0]]),
-     None,
-     "deterministic",
-
-     torch.tensor([[1.0, 1.0, 1.0],
-                   [1.0, 1.0, 1.0],
-                   [1.0, 1.0, 1.0]])),
-    (torch.tensor([[1.0, 1.0, 1.0],
-                   [1.0, 1.0, 1.0],
-                   [1.0, 1.0, 1.0]]),
-     torch.tensor([[-1.0, 1.0, 1.0],
-                   [1.0, -0.8, 1.0],
-                   [1.0, -0.3, 1.0]]),
-     torch.tensor([1.0]),
-     "deterministic",
-     torch.tensor([[2.0, 2.0, 2.0],
-                   [2.0, 2.0, 2.0],
-                   [2.0, 2.0, 2.0]])),
-
-    (torch.tensor([[1.0, 1.0, 1.0],
-                   [1.0, 1.0, 1.0],
-                   [1.0, 1.0, 1.0]]),
-     torch.tensor([[-1.0, 1.0, 1.0],
-                   [1.0, -0.8, 1.0],
-                   [1.0, -0.3, 1.0]]),
-     None,
-     "stochastic",
-     torch.tensor([[3.0, -1.0, -1.0],
-                   [3.0, -1.0, -1.0],
-                   [3.0, -1.0, -1.0]])),
-
-    (torch.tensor([[1.0, 1.0, 1.0],
-                   [1.0, 1.0, 1.0],
-                   [1.0, 1.0, 1.0]]),
-     torch.tensor([[-1.0, 1.0, 1.0],
-                   [1.0, -0.8, 1.0],
-                   [1.0, -0.3, 1.0]]),
-     torch.tensor([1.0]),
-     "stochastic",
-     torch.tensor([[4.0, 0.0, 0.0],
-                   [4.0, 0.0, 0.0],
-                   [4.0, 0.0, 0.0]]))
+    (
+        torch.tensor([[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]),
+        torch.tensor([[-1.0, 1.0, 1.0], [1.0, -0.8, 1.0], [1.0, -0.3, 1.0]]),
+        None,
+        "deterministic",
+        torch.tensor([[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]),
+    ),
+    (
+        torch.tensor([[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]),
+        torch.tensor([[-1.0, 1.0, 1.0], [1.0, -0.8, 1.0], [1.0, -0.3, 1.0]]),
+        torch.tensor([1.0]),
+        "deterministic",
+        torch.tensor([[2.0, 2.0, 2.0], [2.0, 2.0, 2.0], [2.0, 2.0, 2.0]]),
+    ),
+    (
+        torch.tensor([[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]),
+        torch.tensor([[-1.0, 1.0, 1.0], [1.0, -0.8, 1.0], [1.0, -0.3, 1.0]]),
+        None,
+        "stochastic",
+        torch.tensor([[3.0, -1.0, -1.0], [3.0, -1.0, -1.0], [3.0, -1.0, -1.0]]),
+    ),
+    (
+        torch.tensor([[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]),
+        torch.tensor([[-1.0, 1.0, 1.0], [1.0, -0.8, 1.0], [1.0, -0.3, 1.0]]),
+        torch.tensor([1.0]),
+        "stochastic",
+        torch.tensor([[4.0, 0.0, 0.0], [4.0, 0.0, 0.0], [4.0, 0.0, 0.0]]),
+    ),
 ]
 
 
-@ pytest.mark.parametrize("test_input, test_weight, test_bias, test_mode, expected",
-                          forward_test_case)
-def test_forward(fix_seed,
-                 test_input,
-                 test_weight,
-                 test_bias,
-                 test_mode,
-                 expected):
-    assert torch.allclose(input=binarized_linear(test_input, test_weight, test_bias, test_mode),
-                          other=expected,
-                          rtol=1e-04,
-                          atol=1e-04,
-                          equal_nan=True)
+@pytest.mark.parametrize(
+    "test_input, test_weight, test_bias, test_mode, expected", forward_test_case
+)
+def test_forward(fix_seed, test_input, test_weight, test_bias, test_mode, expected):
+    assert torch.allclose(
+        input=binarized_linear(test_input, test_weight, test_bias, test_mode),
+        other=expected,
+        rtol=1e-04,
+        atol=1e-04,
+        equal_nan=True,
+    )
 
 
 indirectly_backward_test_case = [
     # (test_input, test_weight, test_bias, test_mode, expected_weight_grad, expected_input_grad)
-    (torch.tensor([[1.0, 1.0, 1.0]], requires_grad=True),
-     torch.tensor([[-0.8, -0.8, 0.3]],
-                  requires_grad=True),
-     None,
-     "deterministic",
-     torch.tensor([[1., 1., 1.]]),
-     torch.tensor([[-1., -1.,  1.]])
-     ),
-
-    (torch.tensor([[1.0, 1.0, 1.0]], requires_grad=True),
-     torch.tensor([[1.0, -0.8, 0.3]],
-                  requires_grad=True),
-     torch.tensor([1]),
-     "deterministic",
-     torch.tensor([[1., 1., 1.]]),
-     torch.tensor([[1., -1.,  1.]])
-     ),
-
-    (torch.tensor([[1.0, 1.0, 1.0]], requires_grad=True),
-     torch.tensor([[1.0, -0.8, 0.3]],
-                  requires_grad=True),
-     None,
-     "stochastic",
-     torch.tensor([[1., 1., 1.]]),
-     torch.tensor([[-1., -1., -1.]])
-     ),
-
-    (torch.tensor([[1.0, 1.0, 1.0]], requires_grad=True),
-     torch.tensor([[1.0, -0.8, 0.3]],
-                  requires_grad=True),
-     torch.tensor([1]),
-     "deterministic",
-     torch.tensor([[1., 1., 1.]]),
-     torch.tensor([[1., -1., 1.]])
-     )
+    (
+        torch.tensor([[1.0, 1.0, 1.0]], requires_grad=True),
+        torch.tensor([[-0.8, -0.8, 0.3]], requires_grad=True),
+        None,
+        "deterministic",
+        torch.tensor([[1.0, 1.0, 1.0]]),
+        torch.tensor([[-1.0, -1.0, 1.0]]),
+    ),
+    (
+        torch.tensor([[1.0, 1.0, 1.0]], requires_grad=True),
+        torch.tensor([[1.0, -0.8, 0.3]], requires_grad=True),
+        torch.tensor([1]),
+        "deterministic",
+        torch.tensor([[1.0, 1.0, 1.0]]),
+        torch.tensor([[1.0, -1.0, 1.0]]),
+    ),
+    (
+        torch.tensor([[1.0, 1.0, 1.0]], requires_grad=True),
+        torch.tensor([[1.0, -0.8, 0.3]], requires_grad=True),
+        None,
+        "stochastic",
+        torch.tensor([[1.0, 1.0, 1.0]]),
+        torch.tensor([[-1.0, -1.0, -1.0]]),
+    ),
+    (
+        torch.tensor([[1.0, 1.0, 1.0]], requires_grad=True),
+        torch.tensor([[1.0, -0.8, 0.3]], requires_grad=True),
+        torch.tensor([1]),
+        "deterministic",
+        torch.tensor([[1.0, 1.0, 1.0]]),
+        torch.tensor([[1.0, -1.0, 1.0]]),
+    ),
 ]
 
 
-@ pytest.mark.parametrize("test_input, test_weight, test_bias, test_mode, expected_weight_grad, expected_input_grad",
-                          indirectly_backward_test_case)
-def test_backward_indirectly(fix_seed,
-                             test_input,
-                             test_weight,
-                             test_bias,
-                             test_mode,
-                             expected_weight_grad,
-                             expected_input_grad):
+@pytest.mark.parametrize(
+    "test_input, test_weight, test_bias, test_mode, expected_weight_grad, expected_input_grad",
+    indirectly_backward_test_case,
+)
+def test_backward_indirectly(
+    fix_seed,
+    test_input,
+    test_weight,
+    test_bias,
+    test_mode,
+    expected_weight_grad,
+    expected_input_grad,
+):
 
     binarized_linear(test_input, test_weight, test_bias, test_mode).backward()
 
     assert torch.allclose(
         input=test_input.grad,
         other=expected_input_grad,
-
         rtol=1e-04,
         atol=1e-04,
         equal_nan=True,
@@ -175,34 +151,36 @@ def test_backward_indirectly(fix_seed,
 
 directly_backward_test_case = [
     # (saved_tensors, needs_input_grad, grad_output, expected_weight_grad, expected_input_grad, expected_bias_grad)
-    ((torch.tensor([[1., 1., 1.],
-                    [1., 1., 1.],
-                    [1., 1., 1.]], requires_grad=True),
-      torch.tensor([[1., -1.,  1.]]),
-      torch.tensor([1])),
-     (True, True, True, False),
-     torch.tensor([[1.],
-                   [1.],
-                   [1.]]),
-     torch.tensor([[3., 3., 3.]]),
-     torch.tensor([[1., -1.,  1.],
-                   [1., -1.,  1.],
-                   [1., -1.,  1.]]),
-     torch.tensor(3.)
-     )
+    (
+        (
+            torch.tensor(
+                [[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]], requires_grad=True
+            ),
+            torch.tensor([[1.0, -1.0, 1.0]]),
+            torch.tensor([1]),
+        ),
+        (True, True, True, False),
+        torch.tensor([[1.0], [1.0], [1.0]]),
+        torch.tensor([[3.0, 3.0, 3.0]]),
+        torch.tensor([[1.0, -1.0, 1.0], [1.0, -1.0, 1.0], [1.0, -1.0, 1.0]]),
+        torch.tensor(3.0),
+    )
 ]
 
 
-@ pytest.mark.parametrize("saved_tensors, needs_input_grad, grad_output, expected_weight_grad, expected_input_grad, expected_bias_grad",
-                          directly_backward_test_case)
-def test_backward_directly(fix_seed,
-                           saved_tensors,
-                           needs_input_grad,
-                           grad_output,
-                           expected_weight_grad,
-                           expected_input_grad,
-                           expected_bias_grad):
-
+@pytest.mark.parametrize(
+    "saved_tensors, needs_input_grad, grad_output, expected_weight_grad, expected_input_grad, expected_bias_grad",
+    directly_backward_test_case,
+)
+def test_backward_directly(
+    fix_seed,
+    saved_tensors,
+    needs_input_grad,
+    grad_output,
+    expected_weight_grad,
+    expected_input_grad,
+    expected_bias_grad,
+):
     class CTX:
         def __init__(self, saved_tensors, needs_input_grad):
             self.saved_tensors = saved_tensors
@@ -210,8 +188,7 @@ def test_backward_directly(fix_seed,
 
     ctx = CTX(saved_tensors, needs_input_grad)
 
-    input_grad, weight_grad, bias_grad, _ = BinarizedLinear.backward(
-        ctx, grad_output)
+    input_grad, weight_grad, bias_grad, _ = BinarizedLinear.backward(ctx, grad_output)
 
     assert torch.allclose(
         input=input_grad,
